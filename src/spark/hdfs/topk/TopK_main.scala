23ef84e2845e561d8a1d8dd4efd8a9cd66a3ec57
wuxingdexian <dhshenchanggan@163.com>
2014-05-07 14:40:04 +0800

    在yeecloud部署，并修改了代码。自动计算时间差。

--- src/spark/hdfs/topk/TopK_main.scala
+++ src/spark/hdfs/topk/TopK_main.scala
@@ -21,7 +21,8 @@ object topk_main {
    */
   def main(args: Array[String]): Unit = {
     
-    System.out.println("启动计算：" + new Date(System.currentTimeMillis()));//方法一：默认方式输出现在时间
+    val starttime = System.currentTimeMillis()
+    //System.out.println("启动计算：" + new Date(starttime));//方法一：默认方式输出现在时间
 
     val sc = new SparkContext(args(0), "hash1",
       System.getenv("SPARK_HOME"), Seq(System.getenv("SPARK_TEST_JAR")))
@@ -33,26 +34,30 @@ object topk_main {
     // 进行LSH
     val LSH_calculate = line.map { case (key, value: String) => 
       (value.split(" ").map(_.toInt), LSH.LSHTranst(value)) }// value.split(" ").map(_.toInt)
-    println("LSH计算完毕！总共有记录行数：" + line.count)
-    System.out.println("现在时间：" + new Date(System.currentTimeMillis()));
+    println("LSH计算完毕！总共有记录行数：" + LSH_calculate.count)
+    val LSHtime = System.currentTimeMillis()
+    //System.out.println("现在时间：" + new Date(LSHtime));
     
     // 进行BKDRHash  这里控制桶个数，以模多少方式进行
-    val BKDR_hash = LSH_calculate.map { case (key, value) => (BKDR.BKDRHash(value, 14567), key) }
+    val BKDR_hash = LSH_calculate.map { case (key, value) => (BKDR.BKDRHash(value, 5000), key) }
     // BKDR的hash值相同的点归到同一个桶
     val BKDR_group = BKDR_hash.groupByKey
     println("桶生成完毕！总共有桶：" + BKDR_group.count)
-    System.out.println("现在时间：" + new Date(System.currentTimeMillis()));//方法一：默认方式输出现在时间
+    val BKDRtime = System.currentTimeMillis()
+    //System.out.println("现在时间：" + new Date(BKDRtime));//方法一：默认方式输出现在时间
     
     // 求两个桶的笛卡尔积（全连接）  然后过滤掉桶hash值相同的记录
     val bucket_combine = BKDR_group.cartesian(BKDR_group).filter{case (key,value) => (key._1 > value._1)}
     println("桶的笛卡尔积（实际已经完成了桶合并）！总共有桶：" + bucket_combine.count)
-    System.out.println("现在时间：" + new Date(System.currentTimeMillis()))
+    val buckettime = System.currentTimeMillis()
+    //System.out.println("现在时间：" + new Date(buckettime))
     //bucket_combine.cache
     
     // 计算局部top-k 取top-80 
     val Topk_caculate = bucket_combine.map{case(key,value) =>
       ((key._1,value._1),shen.top_k.CallTop_k.pointPair((key._2 ++ value._2).toArray,80))}
     val distance = Topk_caculate.map{case (key,value) => (shen.top_k.Top_KD.DistanceOfTwoPoint(value(0),value(1)),value)}
+    //val topk_globle = distance.sortByKey(true, 1)
     
     //val GlobalTopK_caculate = distance.sortByKey(true, 1)
     /*
@@ -73,7 +78,14 @@ object topk_main {
     // 增加排序，取全局的top-k
     
     println("Top-k计算完毕！总共行数：" + distance.count)
-    System.out.println("结束时间：" + new Date(System.currentTimeMillis()));//方法一：默认方式输出现在时间
+    val topktime = System.currentTimeMillis()
+    //System.out.println("结束时间：" + new Date(topktime) + " " + topktime);//方法一：默认方式输出现在时间
+    
+    println("启动时间——LSH完成时间：" + (LSHtime- starttime) / 1000 )
+    println("LSH完成时间——BKDR桶完成时间：" + (BKDRtime- LSHtime) / 1000 )
+    println("BKDR桶完成时间——buck合并完成时间：" + (buckettime- BKDRtime) / 1000 + " 行数：" + LSH_calculate.count + " 桶数：" + BKDR_group.count + " 桶组合数：" + bucket_combine.count)
+    println("buck合并完成时间——topk完成时间：" + (topktime- buckettime) / 1000)
+    println("总共完成时间：" + (topktime - starttime) / 1000)
     
     /* 测试调用java所写的top-k
     val matrix = Array.ofDim[Int](3, 4) // 三行，四列
